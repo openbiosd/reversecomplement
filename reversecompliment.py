5239f42fadc2c9d39edff7e2f5373ca7076ac2e8
Edmund Fraser <edfraser@yahoo.com>
2016-10-23 16:14:17 -0700

    Initial commit
    Find reverse-compliment of a strand of DNA

---
+++ reversecompliment.py
@@ -0,0 +1,21 @@
+'''
+Find reverse-compliment of a strand of DNA
+'''
+
+alt_map = {'ins':'0'}
+complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+
+def reverse_complement(seq):    
+    for k,v in alt_map.items():
+        seq = seq.replace(k,v)
+    bases = list(seq) 
+    bases = reversed([complement.get(base,base) for base in bases])
+    bases = ''.join(bases)
+    for k,v in alt_map.items():
+        bases = bases.replace(v,k)
+    return bases
+    
+    
+seq = "TCGGinsGCCC"
+print ("Reverse Complement:")
+print (reverse_complement(seq))
